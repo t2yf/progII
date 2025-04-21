9873d02229e6f962f791dcc323a10848f9a8b5e3
t2yf <thamirisyamate@ufpr.br>
2025-04-20 21:51:45 -0300

    [add] auxiliares

---
+++ A1/manip_arc.c
@@ -0,0 +1,27 @@
+#include "manip_arc.h"
+
+//Extrair as informações com o membro fechado (stat)
+void extrair_info_membro(Archiver *archiver, const char *nome_arq, Membro *membro){
+    struct stat info;
+
+    //Verifica se o membro existe
+    if(stat(nome_arq, &info) == -1){
+        perror("Erro: Não foi possível obter informações do membro\n");
+        return;
+    }
+
+    //Puxar os atributos
+    strncpy(membro->nome, nome_arq, MAX_NOME);
+    membro->nome[MAX_NOME -1] = '\0';
+
+    //!!!Tirar os espaços do nome do membro????
+
+    membro->UID = info.st_uid;
+    membro->tamanho_original = info.st_size;
+    membro->tamanho_comprimido = info.st_size;
+    membro->data_modificacao = info.st_mtime;
+
+    ///!!!! O que colocar?
+    membro->ordem = archiver->dir.qtde_membros;
+    membro->offset = 0; //Mudar depois
+}
--- A1/manip_arc.h
+++ A1/manip_arc.h
@@ -6,14 +6,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void extrair_info_membro(Archiver *archiver, const char *nome_arq, Membro *membro);
+
 //verificar se membro já existe no archive
 
 //verificar quantos membros serão inseridos
 
 //extrair informações dos membros
 
-//verificar se archive existe
 
 //atualizar localização dos membros depois de mover ou remover
 
-#endif MANIP_ARC
+#endif 
